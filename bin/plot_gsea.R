c5533be2b1c169ef0f861c139f50152136976dd6
Lucia Conde <l.conde@ucl.ac.uk>
2025-10-23 14:54:56 +0100

    increase plot resolution

--- bin/plot_gsea.R
+++ bin/plot_gsea.R
@@ -35,14 +35,14 @@ data <- mutate(data, GENESET0 = GENESET)
 if (plot == "dot"){
 	#- get plot (based on code from https://www.biostars.org/p/168044/)
 	
-	png("gsea.results.png", width=800)
+	png("gsea.results.png", width=2000, height=2400, res=300)
 	p <- ggplot(data, aes(NES, GENESET)) + 
 	    geom_point(aes(colour=FDR_p, size=RATIO)) +
 	    geom_segment(mapping = aes(yend=GENESET0, xend = 0), size=0.5, colour="gray50") +
 	    geom_point(aes(colour=FDR_p, size=RATIO)) +
 	    scale_color_gradient(limits=c(0, 0.05), low="red", high="white") +
 	    geom_vline(xintercept=0, size=0.5, colour="gray50") +
-	    theme(strip.text.x = element_text(size = 8), 
+	    theme(strip.text.x = element_text(size = 12), 
 		  panel.background=element_rect(fill="gray95", colour="gray95"),
 	          panel.grid.major=element_line(size=0.25,linetype='solid', colour="gray90"), 
 	          panel.grid.minor=element_line(size=0.25,linetype='solid', colour="gray90"),
@@ -53,7 +53,7 @@ if (plot == "dot"){
 	print(p)
 	dev.off()
 }else{
-	png("gsea.results.png", width=800)
+	png("gsea.results.png", width=2000, height=2400, res=300)
         p <- ggplot(data, aes(x=NES, y=fct_reorder(GENESET, -NES))) + 
                 geom_col(aes(fill=FDR_p)) +
                 scale_fill_gradient(limits=c(0, 0.05), low="red", high="white") +
